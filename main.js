57b6e55dbd71a9e6fa847023bd553081a217acdf
uday singh <[udayskai@gmail.com]>
2019-10-05 23:20:45 +0530

    Logic statement

---
+++ main.js
@@ -0,0 +1,31 @@
+"use strict"
+
+
+
+// AND && Operator
+ let Number_1=2018;
+ let Number_2= 1990;
+if (Number_1==2018 &&Number_2==1990) {
+    console.log("true")
+     }
+ else
+ {  
+
+     console.log("false")
+    }
+//TRUE WHEN BOTH VLAUES ARE "TRUE"    
+
+ // tranary method   
+ let result=undefined;
+ result=(Number_1==2018 &&Number_2==190)
+     ?("true")
+     :("false");
+     console.log(result);
+  
+// OR || Operator
+let result1=undefined;
+ result1=(Number_1== 18 || Number_2==190)
+     ?("true")
+     :("false");
+     console.log(result1);
+ // TRUE WHEN ATLEAST ONE VALUE "TRUE"
